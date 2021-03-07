26fea58f29a87ed2ac8a9d9e9388e51c277c8079
metaltony_19 <metaltony_19@hotmail.com>
2021-03-07 01:37:09 -0600

    Routing

--- src/componets/common/Header.tsx
+++ src/componets/common/Header.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavLink} from "react-router-dom";
 
 function Header () {
+    const [active,setActive] = useState("home")
     const activeStyle ={color:"#F15B2A"};
 
+    const presionar =(event:any) => {
+        setActive(event.target.id);
+    }
     return (
         <nav>
-            <NavLink to="/"  id="home" activeStyle={activeStyle}>Home</NavLink>
+            <NavLink to="/" onClick={presionar} id="home"
+             activeStyle={active=="home"?activeStyle:null}>Home</NavLink>
              {" | "}
-            <NavLink to="/about" id="about" activeStyle={activeStyle}>About</NavLink>
+            <NavLink to="/about" id="about" 
+            activeStyle={active=="about"?activeStyle:null}>About</NavLink>
         </nav>
 
     )
