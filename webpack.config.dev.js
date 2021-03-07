ce26d6a2af9b48700c5dea495385ce811cce9aed
metaltony_19 <metaltony_19@hotmail.com>
2021-03-06 22:37:51 -0600

    first commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,43 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development", //environtment 
+  target: "web", 
+  devtool: "cheap-module-source-map", //allows see original code in the browser 
+  entry: "./src/index.tsx", //entry point default page
+  output: { //build in memory en dev env, Webpack requires this value because our html can reference the bundle 
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: { //minimal configuration for dev environtment 
+    stats: "minimal",
+    overlay: true, // overlay any error in the browser
+    historyApiFallback: true, //all request will be sent to index.hmtl
+    disableHostCheck: true, //avoid error in latest version of chrome
+    headers: { "Access-Control-Allow-Origin": "*" }, //any origin
+    https: false
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
