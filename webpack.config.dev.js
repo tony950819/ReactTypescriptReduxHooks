d9c80b3c11e0e325fd5094ad153770946dc96059
metaltony_19 <metaltony_19@hotmail.com>
2021-03-08 17:51:04 -0600

    include files

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -23,6 +23,10 @@ module.exports = {
     https: false
   },
   plugins: [
+    new webpack.DefinePlugin({
+      "process.env.API_URL": JSON.stringify("http://localhost:3001")
+    }),
+
     new HtmlWebpackPlugin({
       template: "src/index.html",
     })
