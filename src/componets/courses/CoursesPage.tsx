0b789fff36336a6f7610d7183fbed995709bf880
metaltony_19 <metaltony_19@hotmail.com>
2021-03-08 18:53:28 -0600

    consulta api

--- src/componets/courses/CoursesPage.tsx
+++ src/componets/courses/CoursesPage.tsx
@@ -1,7 +1,7 @@
-import React,{Dispatch, useState} from 'react';
+import React,{Dispatch,/* useState,*/useEffect} from 'react';
 import {Course} from '../../Models/Models'
-import {connect,useSelector,/*useDispatch*/} from "react-redux"
-import {CreateCourse} from '../../redux/actions/courseActions';
+import {connect,/* useSelector,useDispatch*/} from "react-redux"
+import {CreateCourse,loadCourses} from '../../redux/actions/courseActions';
 //import { bindActionCreators } from 'redux';
 //import { store } from '../../redux/configureStore';
 
@@ -13,17 +13,18 @@ interface props extends StateCoursesPage,DispatchProps {}
 
 interface StateCoursesPage {
     courses:Array<Course>,
-    
-
 }
 interface DispatchProps {
-    CreateCourse:typeof CreateCourse
-}
+    CreateCourse:typeof CreateCourse,
+    loadCourses:typeof loadCourses
 
+}
 
 function CoursePage (courses:props) {
 
     //const dispatch=useDispatch();
+
+    /*
     const test= useSelector <Array<Course>>((state)=>state);
     console.log(test,"test");
     const [course,setCourse]=useState(new Course())
@@ -36,6 +37,15 @@ function CoursePage (courses:props) {
         courses.CreateCourse(course);
         
     }
+    */
+
+    useEffect(() => {
+        console.log("cargando");
+        //dispatch(loadCourses())
+       //loadCourses();
+       courses.loadCourses();
+    },[]);
+
     const ShowCourses = () => {
             
         let coursesTemp:Array<any> = new Array<any>();
@@ -51,13 +61,18 @@ function CoursePage (courses:props) {
     }
     return (
         <>
-         <form onSubmit={handleSubmit}>
+        {/*  
+           <form onSubmit={handleSubmit}>
              <h2>Courses</h2>
              <h3>Add course</h3>
              <input type="text" onChange={handleChange} value={course.title}/>
              <input type="submit" value="save"></input>
+            </form>
+        
+        */}
+      
              {ShowCourses()}
-         </form>
+     
         </>
     )
 }
@@ -69,5 +84,6 @@ const  mapStateToProps  = (state:any)=>({
 
 const mapDispatchToprops =(dispatch:Dispatch<any>) => ({
     CreateCourse: (course:Course) => dispatch(CreateCourse(course)),
+    loadCourses: () =>dispatch(loadCourses())
 })
 export default connect(mapStateToProps ,mapDispatchToprops) (CoursePage);
